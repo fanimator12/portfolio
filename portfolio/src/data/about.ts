2716aa6b76781bce173478c2b6ffd189b7dcd56b
fanimator12 <torok.franciska.leonora@gmail.com>
2023-01-13 13:17:18 +0100

    update profile

--- portfolio/src/data/about.ts
+++ portfolio/src/data/about.ts
@@ -1,7 +1,7 @@
 const about = [
   {
     name: 'Franciska Török',
-    profile: "https://scontent.fbll1-1.fna.fbcdn.net/v/t39.30808-6/324398803_1203609737249792_5412873250970377257_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=5T-zeG1ol8IAX-w10KE&tn=BzX-jb831eTpSBl5&_nc_ht=scontent.fbll1-1.fna&oh=00_AfBQspPN7UkKpQLsruRW3y0FKlrd_aqGXbKv6xP6WT1U1w&oe=63BECA38",
+    profile: "src/photos/profile.jpg",
     age: new Date().getFullYear() - 2000,
     education: [
       {
