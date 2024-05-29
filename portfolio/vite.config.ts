65522fb492a34dbcd3f5bb8142e9f7d0fbfb0ef2
fanimator12 <torok.franciska.leonora@gmail.com>
2024-05-29 21:56:37 +0200

    changes chunksize

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   server: {
     port: 8080,
     watch: {
-      usePolling: true
-  }
+      usePolling: true,
+    },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1000, 
+  },
+});
