4c42881bd32a06953375573874ba96e1d5cfc437
Franciska Török <torok.franciska.leonora@gmail.com>
2024-08-04 20:28:42 +0200

    flake8

--- backend/main.py
+++ backend/main.py
@@ -59,19 +59,21 @@ def sync_s3_with_db():
     try:
         print("Starting S3 to DB sync process...")
         response = s3.list_objects_v2(Bucket=BUCKET_NAME)
-        if 'Contents' in response:
-            for obj in response['Contents']:
-                filename = obj['Key']
+        if "Contents" in response:
+            for obj in response["Contents"]:
+                filename = obj["Key"]
                 file_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{filename}"
 
                 print(f"Checking if {filename} exists in the database...")
-                existing_photo = session.query(Photo).filter(Photo.filename == filename).first()
+                existing_photo = (
+                    session.query(Photo).filter(Photo.filename == filename).first()
+                )
                 if not existing_photo:
                     print(f"Inserting {filename} into the database...")
                     new_photo = Photo(filename=filename, url=file_url)
                     session.add(new_photo)
                     print(f"Added {filename} to the database.")
-        
+
         session.commit()
         print("Database sync completed and committed.")
     except Exception as e:
@@ -88,8 +90,12 @@ async def get_photos():
     try:
         photos = session.query(Photo).all()
         # Filter photos to only include filenames that contain numbers
-        filtered_photos = [photo for photo in photos if re.search(r'\d', photo.filename)]
-        return [{"filename": photo.filename, "url": photo.url} for photo in filtered_photos]
+        filtered_photos = [
+            photo for photo in photos if re.search(r"\d", photo.filename)
+        ]
+        return [
+            {"filename": photo.filename, "url": photo.url} for photo in filtered_photos
+        ]
     finally:
         session.close()
 
@@ -105,14 +111,14 @@ async def get_photo(filename: str):
 
     # Generate the URL to the photo in S3
     file_url = s3.generate_presigned_url(
-        'get_object',
-        Params={'Bucket': BUCKET_NAME, 'Key': filename},
-        ExpiresIn=3600  # URL expiration time in seconds
+        "get_object",
+        Params={"Bucket": BUCKET_NAME, "Key": filename},
+        ExpiresIn=3600,
     )
 
     return {"filename": filename, "url": file_url}
 
 
 if __name__ == "__main__":
-    time.sleep(5)  # Ensure the database is ready
+    time.sleep(5)
     sync_s3_with_db()
