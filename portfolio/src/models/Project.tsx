da10563327c2ff23f34c45cc78891a0d31eadacc
Franciska Török <torok.franciska.leonora@gmail.com>
2024-08-04 20:13:18 +0200

    u

--- portfolio/src/models/Project.tsx
+++
@@ -1,9 +0,0 @@
-export interface Project {
-    id: number,
-    image: string,
-    title: string,
-    type: string,
-    url: string,
-    description: string,
-    technologies: string,
-}
--- portfolio/src/models/ProjectBox.tsx
+++
@@ -1,9 +0,0 @@
-export interface ProjectBox {
-    id: number,
-    image: string,
-    title: string,
-    type: string,
-    url: string,
-    description: string,
-    technologies: string,
-}
